3efc136008e5dbc335103ed35bc0b3d418386c47
DirtySt <sergeikashtan1337@gmail.com>
2023-08-16 20:43:07 +0300

    Initial commit

---
+++ task1.js
@@ -0,0 +1,17 @@
+let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let one = 1;
+let ten = 10;
+let minus = -999;
+let numbers = 123;
+let pi = 3.14;
+let two = 2.7;
+let sixteen = 16;
+let bool1 = true;
+let bool2 = false;
+let arr = [hello, owu, com, ua, one, ten, minus, numbers, pi, two, sixteen, bool1, bool2];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
---
+++ task2.js
@@ -0,0 +1,5 @@
+let firstName = 'Olena';
+let lastName = 'Ivanovna';
+let middleName = 'Hrishchenko';
+let person = firstName + " " + middleName + " " + lastName;
+console.log(person)
